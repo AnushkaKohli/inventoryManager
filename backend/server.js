9ce04e873e6f73f013b3550507c3efa381fd29ef
Rohit <rohitranjan2702@gmail.com>
2023-04-22 02:25:32 +0530

    port changed

--- backend/server.js
+++ backend/server.js
@@ -33,24 +33,12 @@ app.get("/", (req, res) => {
 app.use(errorHandler);
 
 //Connecting to mongoDB
-<<<<<<< HEAD
-const PORT = process.env.PORT || 3000;
-mongoose.connect(process.env.MONGO_URI)
-    .then(function(){
-        app.listen(PORT, function(){
-            console.log(`Server started on port ${PORT}`);
-        })
-    })
-    .catch(function(err){
-        console.log(err);
-=======
 const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI)
   .then(function () {
     app.listen(PORT, function () {
       console.log(`Server started on port ${PORT}`);
->>>>>>> d1fd1cf72ec5347ebccefad06cc8d349a850f19c
     });
   })
   .catch(function (err) {
